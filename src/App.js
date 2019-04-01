3bb10ec347d15abf4d2186a6347af8417109d85d
Meng Han <menghan@jetco.com.hk>
2019-04-01 17:33:45 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I was change on the Feature branch.
           </a>
         </header>
       </div>
